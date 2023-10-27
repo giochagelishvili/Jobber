01c6cdade34d88a123c1bb0b8c96b90beaa3e023
Giorgi Chagelishvili <chagx116@gmail.com>
2023-10-27 15:53:37 +0400

    import font / create nav-menu

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,13 @@
 <template>
   <img src="@/assets/logo.svg" width="120" />
+
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Explore</li>
+      <li>About Us</li>
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -7,3 +15,18 @@ export default {
   name: 'HeaderComponent'
 }
 </script>
+
+<style scoped>
+ul {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+
+  list-style: none;
+}
+
+li {
+  font-size: 18px;
+  letter-spacing: 2px;
+}
+</style>
