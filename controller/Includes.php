ffce26e50d7d67876a1382819c22fd624a82a25c
Giorgi Chagelishvili <chagx116@gmail.com>
2023-10-29 17:09:44 +0400

    create validator class

---
+++ controller/Includes.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once("../classes/Validator.classes.php");
--- controller/JobController.php
+++ controller/JobController.php
@@ -1,12 +1,46 @@
 <?php
 
 require_once('Headers.php');
-// require_once('Includes.php');
+require_once('Includes.php');
 
 $data;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = decodeData();
+}
+
+if (isset($data['action'])) {
+    $action = $data['action'];
+
+    switch ($action) {
+        case 'uploadJob':
+            if (isset($data['formData'])) {
+                uploadJob($data['formData']);
+            }
+            break;
+    }
+}
+
+function uploadJob(array $formData)
+{
+    $errors = [];
+
+    if (
+        !isset($formData['jobTitle']) || $formData['jobTitle'] == '' ||
+        !isset($formData['jobLocation']) || $formData['jobLocation'] == '' ||
+        !isset($formData['jobType']) || $formData['jobType'] == '' ||
+        !isset($formData['jobCategory']) || $formData['jobCategory'] == '' ||
+        !isset($formData['jobDescription']) || $formData['jobDescription'] == '' ||
+        !isset($formData['jobRequirements']) || $formData['jobRequirements'] == '' ||
+        !isset($formData['salaryType']) || $formData['salaryType'] == '' ||
+        !isset($formData['salaryAmount']) || $formData['salaryAmount'] == ''
+    ) {
+        array_push($errors, "Please fill out every field.");
+        echo json_encode($errors);
+        exit();
+    }
+
+    $validator = new Validator();
 }
 
 // Decodes received data
